722f8c0476c1f608edf94a6daab3ffaee507d592
fparadas <felipegparadas@gmail.com>
2019-10-16 11:26:51 -0300

    corrigindo bugs

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -4,15 +4,14 @@
   <v-toolbar app clipped-left class="elevation-2" height="70px">
     <v-toolbar-title>
       <v-toolbar-side-icon v-on:click="listeners.click"></v-toolbar-side-icon>
-      <v-img 
+    </v-toolbar-title>
+     <v-img 
         src="../../assets/imagem1.png"
         aspect-ratio="1"
         max-width="500"
         max-height="300"
       >
       </v-img>
-    </v-toolbar-title>
-    <span class="toolbar-title">Nanã</span>
     <v-spacer></v-spacer>
   </v-toolbar>
 </template> 
